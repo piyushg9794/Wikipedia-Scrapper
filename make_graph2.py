34e7eefa774fbc0082f155ed2434082c5bac5069
Piyush Gupta <piyushg9794@gmail.com>
2020-04-09 13:03:15 +0530

    drawing graph using networkx.py

---
+++ make_graph2.py
@@ -0,0 +1,10 @@
+import networkx as nx
+
+def make_graph2(node_list, node_a, node_b):
+    
+    grp2.add_node(node_a)
+    
+    if node_b!= None:
+
+        grp2.add_edge(*(node_a, node_b))
+        
